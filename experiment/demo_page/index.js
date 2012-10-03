fd4e0b4893eeb532a9f1ccd2f510f0da2c6dc288
simplemx <simplemx@126.com>
2012-10-03 23:14:30 +0800

    add for blog content

---
+++ experiment/demo_page/index.js
@@ -0,0 +1,35 @@
+var postList = [
+	{
+		title:"test1",
+		date : "2012-10-01",
+		url:"post1.html"
+	},
+	{
+		title:"test2",
+      		date:"2012-10-12",
+      		url:"post2.html"
+	}
+];
+var gotoBlog = function(url){
+	var callback = function(content){
+		$(".blog_list").remove();
+		$(".main_content").append(content);
+	};
+	var param = {
+		url : url,
+		success : callback,
+		dataType : "html"
+	};
+	$.ajax(param);
+	
+};
+
+$(function(){
+	var context = {
+		"blog_list": postList
+	}
+		
+	var template = $("#blog_list_template").html();
+	var html = Mustache.to_html(template, context);
+	$(".main_content").append(html);	
+});
